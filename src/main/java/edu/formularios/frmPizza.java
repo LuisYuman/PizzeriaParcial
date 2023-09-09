382d215c9110bb4d5b9fc58d16844f1f33137094
Luis Yuman <lyuman737@gmail.com>
2023-09-09 12:46:00 -0600

    Initial commit

---
+++ src/main/java/edu/formularios/frmPizza.java
@@ -0,0 +1,76 @@
+package edu.formularios;
+
+import edu.pizza.base.Pizza;
+import edu.pizza.base.Topping;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
+
+public class frmPizza {
+    private JPanel jPanelPrincipal;
+    private JComboBox comboBoxToppings;
+    private JTextField txtPizza;
+    private JButton btnAddIngrediente;
+    private JLabel lblTotal;
+    private JList lista1;
+    private JButton btbPreparPizza;
+
+    private List<Topping> ingredientes = new ArrayList<>();
+
+    private DefaultListModel modeloLista = new DefaultListModel();
+
+    private double total = 0.0;
+    public JPanel getjPanelPrincipal() {
+        return jPanelPrincipal;
+    }
+
+
+    public frmPizza() {
+        cargarToppings();
+        btnAddIngrediente.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Topping ingrediente = (Topping) comboBoxToppings.getSelectedItem();
+
+                modeloLista.addElement(ingrediente);
+                lista1.setModel(modeloLista);
+                total += ingrediente.getPrecio();
+                lblTotal.setText(String.valueOf(total));
+
+            }
+        });
+
+
+        btbPreparPizza.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Pizza pizza = new Pizza(txtPizza.getText());
+                Topping topi;
+                for (int i = 0; i< lista1.getModel().getSize(); i++){
+                    topi = (Topping) lista1.getModel().getElementAt(i);
+                    pizza.addTopping(topi);
+                }
+                pizza.prepare();
+            }
+        });
+    }
+
+
+
+    //cargar toppings
+    private void cargarToppings() {
+        ingredientes.add(new Topping("Champiñones", 10));
+        ingredientes.add(new Topping("Mozzarella", 11));
+        ingredientes.add(new Topping("Cebolla", 6));
+        ingredientes.add(new Topping("Tomate", 9));
+        ingredientes.add(new Topping("Pepperoni", 15));
+        ingredientes.add(new Topping("Bacon", 18));
+
+        DefaultComboBoxModel model = new DefaultComboBoxModel(ingredientes.toArray());
+        comboBoxToppings.setModel(model);
+
+    }
+}
